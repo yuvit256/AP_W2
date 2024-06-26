ac1063da8ff903a42be20a7fd15f49ca968539b4
ron12120 <76705730+ron12120@users.noreply.github.com>
2024-06-26 19:56:40 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main() {
     int how_much_threads = 0;
     int how_much_primes = 0;
 
-    how_much_threads = 1000;
+    how_much_threads = 8;
 
     create_list_of_primes_for_threads();
     how_much_primes = task_for_threads_to_check_primes(how_much_threads);
