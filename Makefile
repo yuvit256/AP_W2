e128c0077d4596375d5ac58708c62ac87548acfb
ron12120 <76705730+ron12120@users.noreply.github.com>
2024-06-29 16:26:35 +0300

    Add files via upload

--- Makefile
+++ Makefile
@@ -1,22 +1,15 @@
+.PHONY: all
+all: generator primeCounter MyPrimeCounter
 
-CC = gcc
-CFLAGS = -Wall -Werror -g
-TARGETS = primesCounter randomGenerator
-MY_PROGRAM_OBJS = main.o MyPrimeCounter.o
-GENERATOR_OBJS = generator.o MyPrimeCounter.o
-all: $(TARGETS)
-primesCounter: $(MY_PROGRAM_OBJS)
-	$(CC) $(CFLAGS) -o primesCounter $(MY_PROGRAM_OBJS) -lm
-randomGenerator: $(GENERATOR_OBJS)
-	$(CC) $(CFLAGS) -o randomGenerator $(GENERATOR_OBJS) -lm
-main.o: main.c MyPrimeCounter.h
-	$(CC) $(CFLAGS) -c main.c
-MyPrimeCounter.o: MyPrimeCounter.c MyPrimeCounter.h
-	$(CC) $(CFLAGS) -c MyPrimeCounter.c
-generator.o: generator.c
-	$(CC) $(CFLAGS) -c generator.c
+generator:  generator.c
+	gcc -o randomGenerator generator.c
+
+primeCounter:	primeCounter.c
+	gcc -o primeCounter primeCounter.c
+
+MyPrimeCounter:  MyPrimeCounter.c
+	gcc -o MyPrimeCounter MyPrimeCounter.c -lm
+
+.PHONY: clean
 clean:
-	rm -f $(TARGETS) *.o
-
-.PHONY: all clean
-
+	-rm randomGenerator primeCounter MyPrimeCounter 2>/dev/null
--- MyPrimeCounter.c
+++ MyPrimeCounter.c
@@ -1,123 +1,122 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <stdatomic.h>
+#include <math.h>
+
+typedef struct
+{
+    int *numbers;
+    int start;
+    int end;
+    int primeCount;
+} ThreadData;
+// bool isPrime(int n)
+// {
+//     if (n <= 1)
+//     {
+//         return false;
+//     }
+//     if (n <= 3)
+//     {
+//         return true;
+//     }
+//     if (n % 2 == 0 || n % 3 == 0)
+//     {
+//         return false;
+//     }
+//     int sqrt_n = sqrt(n);
+//     for (int i = 5; i <= sqrt_n; i += 6)
+//     {
+//         if (n % i == 0 || n % (i + 2) == 0)
+//         {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
 
 
-
-#include "MyPrimeCounter.h"
-
-// Global variables
-int array_size_of_primes = 0;
-int *array_of_prime_numbers = NULL;
-int *how_much_threads = NULL;
-pthread_t *thread_list = NULL;
-
-void cleanup() {
-    free(array_of_prime_numbers);
-    free(how_much_threads);
-    free(thread_list);
-}
-
-bool check_if_number_is_prime_standard(int check_if_prime, int where_to_start) {
-    int threshold = (int)sqrt(check_if_prime) + 1;
-    for (int i = where_to_start; i <= threshold; i++) {
-        if (check_if_prime % i == 0) {
+//the is prime function that provided
+bool isPrime(int n)
+{
+    if (n <= 1)
+    {
+        return false;
+    }
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
             return false;
         }
     }
     return true;
 }
 
-void create_list_of_primes_for_threads() {
-    int i, j;
-    bool *list_of_status = (bool*)malloc(PRIMES_ARRAY_LIMIT * sizeof(bool));
-    if (list_of_status == NULL) {
-        perror("Failed to allocate memory for list_of_status");
-        exit(EXIT_FAILURE);
-    }
+atomic_int totalPrimes = 0;
 
-    for (i = 0; i < PRIMES_ARRAY_LIMIT; i++) {
-        list_of_status[i] = true;
-    }
-
-    int threshold = (int)sqrt(PRIMES_ARRAY_LIMIT) + 1;
-    for (i = 2; i < threshold; i++) {
-        if (list_of_status[i]) {
-            for (j = i * i; j < PRIMES_ARRAY_LIMIT; j += i) {
-                list_of_status[j] = false;
-            }
-        }
-    }
-
+void *countPrimes(void *arg)
+{
+    ThreadData *data = (ThreadData *)arg;
     int count = 0;
-    for (i = 2; i < PRIMES_ARRAY_LIMIT; i++) {
-        if (list_of_status[i]) {
+    for (int i = data->start; i < data->end; i++)
+    {
+        if (isPrime(data->numbers[i]))
+        {
             count++;
         }
     }
+    atomic_fetch_add(&totalPrimes, count);
+    pthread_exit(NULL);
+}
 
-    array_of_prime_numbers = (int*)malloc(count * sizeof(int));
-    if (array_of_prime_numbers == NULL) {
-        perror("Failed to allocate memory for array_of_prime_numbers");
-        exit(EXIT_FAILURE);
-    }
+int main()
+{
+    int optimal_threads = 6; // Fixed number of threads (adjust as needed)
+    int *numbers = NULL;
+    int num, count = 0, capacity = 45830;
 
-    int index = 0;
-    for (i = 2; i < PRIMES_ARRAY_LIMIT; i++) {
-        if (list_of_status[i]) {
-            array_of_prime_numbers[index++] = i;
+    numbers = (int *)malloc(capacity * sizeof(int));
+
+    while (scanf("%d", &num) != EOF)
+    {
+        if (count == capacity)
+        {
+            capacity *= 2;
+            numbers = (int *)realloc(numbers, capacity * sizeof(int));
+            if (numbers == NULL)
+            {
+                perror("Memory allocation failed");
+                exit(1);
+            }
         }
+        numbers[count++] = num;
     }
-    array_size_of_primes = count;
-    free(list_of_status);
-}
-bool isPrime(int a_number) {
-    for (int i = 0; i < array_size_of_primes; i++) {
-        if (a_number % array_of_prime_numbers[i] == 0) {
-            return false;
-        }
+
+    pthread_t threads[optimal_threads];
+    ThreadData threadData[optimal_threads];
+    int chunkSize = (count + optimal_threads - 1) / optimal_threads;
+
+    for (int i = 0; i < optimal_threads; i++)
+    {
+        threadData[i].numbers = numbers;
+        threadData[i].start = i * chunkSize;
+        threadData[i].end = (i == optimal_threads - 1) ? count : (i + 1) * chunkSize;
+        pthread_create(&threads[i], NULL, countPrimes, &threadData[i]);
     }
-    return check_if_number_is_prime_standard(a_number, array_of_prime_numbers[array_size_of_primes - 1] + 1);
-}
 
-int count_how_many_primes_were() {
-    int curr_number_scanned;
-    int how_much_primes = 0;
-
-    while (scanf("%d", &curr_number_scanned) != EOF) {
-        if (isPrime(curr_number_scanned)) {
-            how_much_primes++;
-        }
+    for (int i = 0; i < optimal_threads; i++)
+    {
+        pthread_join(threads[i], NULL);
     }
-    return how_much_primes;
-}
 
-void* thread_task_for_find_and_count_primes(void *arguments) {
-    int *workerCounter = (int*)arguments;
-    *workerCounter = count_how_many_primes_were();
-    return NULL;
+    printf("%d total primes.\n", totalPrimes);
+
+    free(numbers);
+
+    return 0;
 }
 
-int task_for_threads_to_check_primes(int threads) {
-    int total_primes = 0;
 
-    how_much_threads = (int*)malloc(threads * sizeof(int));
-    thread_list = (pthread_t*)malloc(threads * sizeof(pthread_t));
-
-    if (how_much_threads == NULL || thread_list == NULL) {
-        perror("Failed to allocate memory for threads");
-        exit(EXIT_FAILURE);
-    }
-
-    for (int i = 0; i < threads; i++) {
-        how_much_threads[i] = 0;
-        pthread_create(&thread_list[i], NULL, thread_task_for_find_and_count_primes, &how_much_threads[i]);
-    }
-
-    for (int i = 0; i < threads; i++) {
-        pthread_join(thread_list[i], NULL);
-    }
-
-    for (int i = 0; i < threads; i++) {
-        total_primes += how_much_threads[i];
-    }
-
-    return total_primes;
-}
