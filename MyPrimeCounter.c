1fbe7a9d5f1627c9512a1ac69aaf5f192bf0bb29
yuvit256 <117669404+yuvit256@users.noreply.github.com>
2024-06-30 17:19:03 +0300

    Add files via upload

--- MyPrimeCounter.c
+++ MyPrimeCounter.c
@@ -7,38 +7,21 @@
 
 typedef struct
 {
-    int *numbers;
-    int start;
-    int end;
-    int primeCount;
+    int totalPrimes;
+    pthread_mutex_t prime_lock;
 } ThreadData;
-bool isPrime(int n)
-{
-    if (n <= 1)
-    {
-        return false;
-    }
-    if (n <= 3)
-    {
-        return true;
-    }
-    if (n % 2 == 0 || n % 3 == 0)
-    {
-        return false;
-    }
-    int sqrt_n = sqrt(n);
-    for (int i = 5; i <= sqrt_n; i += 6)
-    {
-        if (n % i == 0 || n % (i + 2) == 0)
-        {
-            return false;
-        }
-    }
-    return true;
+
+bool isPrime(int n) {
+  if (n <= 2 || n % 2 == 0) 
+    return n == 2;
+  for (int i = 3; i * i <= n; i += 2)
+    if (n % i == 0)
+      return false;
+  return true;
 }
 
 
-//the is prime function that provided
+// the is prime function that provided
 
 // bool isPrime(int n)
 // {
@@ -56,56 +39,33 @@ bool isPrime(int n)
 //     return true;
 // }
 
-atomic_int totalPrimes = 0;
-
 void *countPrimes(void *arg)
 {
     ThreadData *data = (ThreadData *)arg;
-    int count = 0;
-    for (int i = data->start; i < data->end; i++)
+    int num = 0;
+    while (scanf("%d", &num) != EOF)
     {
-        if (isPrime(data->numbers[i]))
+        if (isPrime(num))
         {
-            count++;
+            pthread_mutex_lock(&data->prime_lock);
+            data->totalPrimes++;
+            pthread_mutex_unlock(&data->prime_lock);
         }
     }
-    atomic_fetch_add(&totalPrimes, count);
     pthread_exit(NULL);
 }
 
 int main()
 {
     int optimal_threads = 6; // Fixed number of threads (adjust as needed)
-    int *numbers = NULL;
-    int num, count = 0, capacity = 45830;
-
-    numbers = (int *)malloc(capacity * sizeof(int));
-
-    while (scanf("%d", &num) != EOF)
-    {
-        if (count == capacity)
-        {
-            capacity *= 2;
-            numbers = (int *)realloc(numbers, capacity * sizeof(int));
-            if (numbers == NULL)
-            {
-                perror("Memory allocation failed");
-                exit(1);
-            }
-        }
-        numbers[count++] = num;
-    }
-
     pthread_t threads[optimal_threads];
-    ThreadData threadData[optimal_threads];
-    int chunkSize = (count + optimal_threads - 1) / optimal_threads;
+    ThreadData threadData;
+    threadData.totalPrimes = 0;
+    pthread_mutex_init(&threadData.prime_lock, NULL);
 
     for (int i = 0; i < optimal_threads; i++)
     {
-        threadData[i].numbers = numbers;
-        threadData[i].start = i * chunkSize;
-        threadData[i].end = (i == optimal_threads - 1) ? count : (i + 1) * chunkSize;
-        pthread_create(&threads[i], NULL, countPrimes, &threadData[i]);
+        pthread_create(&threads[i], NULL, countPrimes, &threadData);
     }
 
     for (int i = 0; i < optimal_threads; i++)
@@ -113,11 +73,9 @@ int main()
         pthread_join(threads[i], NULL);
     }
 
-    printf("%d total primes.\n", totalPrimes);
+    printf("%d total primes.\n", threadData.totalPrimes);
 
-    free(numbers);
+    pthread_mutex_destroy(&threadData.prime_lock);
 
     return 0;
 }
-
-
